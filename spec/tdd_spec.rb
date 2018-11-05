f602a2616bd03d8f07e5e6c671b18dc20fcf5a59
Jorge González Cabrera <alu0101038490@ull.edu.es>
2018-11-05 15:50:41 +0000

    Prueba fallida de inicialización de la clase Etiqueta

--- spec/tdd_spec.rb
+++ spec/tdd_spec.rb
@@ -7,3 +7,9 @@ RSpec.describe Tdd do
     expect(false).to eq(true)
   end
 end
+
+RSpec.describe Etiqueta do
+	it "se inicializa correctamente" do
+		Etiqueta.new("Magdalena",21.7,2.6,54.9,29.0,4.8,1.15,0.0,0.0,0.0,0.0,0.0,0.0,0.0)
+	end
+end
