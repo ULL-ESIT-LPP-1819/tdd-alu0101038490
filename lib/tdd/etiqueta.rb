193a56870207d6134e4077a14cb0eb097b07dd73
Jorge González Cabrera <alu0101038490@ull.edu.es>
2018-11-05 16:04:20 +0000

    Añadida la posibilidad para inicializar la clase Etiqueta con todos sus atributos y así corregir test anterior.

--- lib/tdd/etiqueta.rb
+++ lib/tdd/etiqueta.rb
@@ -1,2 +1,29 @@
 class Etiqueta 
+
+	attr_accessor :nombre, :grasas, :grasasSaturadas, :hidratosDeCarbono, 
+		:azucares, :proteinas, :sal, :grasasMonoinsaturadas,
+	       	:grasasPoliinsaturadas, :polialcoholes, :almidon, :fibraAlimentaria, 
+		:vitaminas, :minerales
+
+	def initialize(nombre, grasas, grasasSaturadas, hidratosDeCarbono, 
+                azucares, proteinas, sal, grasasMonoinsaturadas, 
+                grasasPoliinsaturadas, polialcoholes, almidon, fibraAlimentaria, 
+                vitaminas, minerales)
+
+		@nombre  = nombre
+		@grasas = grasas
+		@grasasSaturadas = grasasSaturadas
+		@hidratosDeCarbono = hidratosDeCarbono
+		@azucares = azucares
+		@proteinas = proteinas
+		@sal = sal
+		@grasasMonoinsaturadas = grasasMonoinsaturadas
+		@grasasPoliinsaturadas = grasasPoliinsaturadas
+		@polialcoholes = polialcoholes
+		@almidon = almidon
+		@fibraAlimentaria = fibraAlimentaria
+		@vitaminas = vitaminas
+		@minerales = minerales
+	end
+
 end
