b2cdd5049a680c1e62d4e0ef9c32513155de2082
Jorge González Cabrera <alu0101038490@ull.edu.es>
2018-11-06 08:47:03 +0000

    Prueba aún fallida a la espera de corrección de la práctica.

--- lib/tdd/etiqueta.rb
+++ lib/tdd/etiqueta.rb
@@ -26,4 +26,17 @@ class Etiqueta
 		@minerales = minerales
 	end
 
+	def valorEnergeticoEnKJ
+		valor = 0.0
+		valor += (27 * @grasas)
+		valor += (8 * @fibraAlimentaria)
+		valor += (17 * @proteinas)
+		valor += (25 * @sal)
+		valor.round(0)
+	end
+
+	def valorEnergeticoEnKcal
+		valorEnergeticoEnKJ / 4.184
+	end
+
 end
