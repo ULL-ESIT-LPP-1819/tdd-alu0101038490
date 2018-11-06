513d2d237be75b853640138d0a4c9e8556a8ab4c
Jorge González Cabrera <alu0101038490@ull.edu.es>
2018-11-06 10:52:30 +0000

    Pruebas fallidas de la inicialización de la etiqueta cuando se divide en porciones.

--- spec/tdd_spec.rb
+++ spec/tdd_spec.rb
@@ -9,41 +9,44 @@ RSpec.describe Tdd do
 end
 
 RSpec.describe Etiqueta do
-	it "se inicializa correctamente" do
-		prueba = Etiqueta.new("Magdalena",21.7,2.6,54.9,29.0,4.8,1.15,0.0,0.0,0.0,0.0,0.0,0.0,0.0)
-		expect(prueba.nombre).to eq("Magdalena")
-		expect(prueba.grasas).to eq(21.7)
-		expect(prueba.grasasSaturadas).to eq(2.6)
-		expect(prueba.hidratosDeCarbono).to eq(54.9)
-		expect(prueba.azucares).to eq(29.0)
-		expect(prueba.proteinas).to eq(4.8)
-		expect(prueba.sal).to eq(1.15)
-		expect(prueba.grasasMonoinsaturadas).to eq(0.0)
-		expect(prueba.grasasPoliinsaturadas).to eq(0.0)
-		expect(prueba.polialcoholes).to eq(0.0)
-		expect(prueba.almidon).to eq(0.0)
-		expect(prueba.fibraAlimentaria).to eq(0.0)
-		expect(prueba.vitaminas).to eq(0.0)
-		expect(prueba.minerales).to eq(0.0)
-	end
 
-	it "calcula correctamente los valores energéticos" do
-		prueba = Etiqueta.new("Magdalena",21.7,2.6,54.9,29.0,4.8,1.15,0.0,0.0,0.0,0.0,0.0,0.0,0.0)
-		expect(prueba.valorEnergeticoEnKJ).to eq(1829)
-		expect(prueba.valorEnergeticoEnKcal).to eq(437)
-	end
+	context "cuando no se divide en porciones" do
+		
+		it "se inicializa correctamente" do
+			prueba = Etiqueta.new("Magdalena",21.7,2.6,54.9,29.0,4.8,1.15,0.0,0.0,0.0,0.0,0.0,0.0,0.0)
+			expect(prueba.nombre).to eq("Magdalena")
+			expect(prueba.grasas).to eq(21.7)
+			expect(prueba.grasasSaturadas).to eq(2.6)
+			expect(prueba.hidratosDeCarbono).to eq(54.9)
+			expect(prueba.azucares).to eq(29.0)
+			expect(prueba.proteinas).to eq(4.8)
+			expect(prueba.sal).to eq(1.15)
+			expect(prueba.grasasMonoinsaturadas).to eq(0.0)
+			expect(prueba.grasasPoliinsaturadas).to eq(0.0)
+			expect(prueba.polialcoholes).to eq(0.0)
+			expect(prueba.almidon).to eq(0.0)
+			expect(prueba.fibraAlimentaria).to eq(0.0)
+			expect(prueba.vitaminas).to eq(0.0)
+			expect(prueba.minerales).to eq(0.0)
+		end
 
-	it "calcula correctamente los valores de la tabla" do 
-		prueba = Etiqueta.new("Magdalena",21.7,2.6,54.9,29.0,4.8,1.15,0.0,0.0,0.0,0.0,0.0,0.0,0.0)
-		#expect(prueba.porcentajeValorEnergetico).to eq()
-                expect(prueba.porcentajeGrasas).to eq(31)
-                expect(prueba.porcentajeSaturadas).to eq(13)
-                expect(prueba.porcentajeHidratos).to eq(21)
-                expect(prueba.porcentajeAzucares).to eq(32)
-                expect(prueba.porcentajeProteinas).to eq(10)
-                expect(prueba.porcentajeSal).to eq(19)
+		it "calcula correctamente los valores energéticos" do
+			prueba = Etiqueta.new("Magdalena",21.7,2.6,54.9,29.0,4.8,1.15,0.0,0.0,0.0,0.0,0.0,0.0,0.0)
+			expect(prueba.valorEnergeticoEnKJ).to eq(1829)
+			expect(prueba.valorEnergeticoEnKcal).to eq(437)
+		end
 
-		expect(prueba.to_s).to eq("\tPor 100g\tIR (por 100g)\n" +
+		it "calcula correctamente los valores de la tabla" do 
+			prueba = Etiqueta.new("Magdalena",21.7,2.6,54.9,29.0,4.8,1.15,0.0,0.0,0.0,0.0,0.0,0.0,0.0)
+			#expect(prueba.porcentajeValorEnergetico).to eq()
+                	expect(prueba.porcentajeGrasas).to eq(31)
+                	expect(prueba.porcentajeSaturadas).to eq(13)
+                	expect(prueba.porcentajeHidratos).to eq(21)
+                	expect(prueba.porcentajeAzucares).to eq(32)
+                	expect(prueba.porcentajeProteinas).to eq(10)
+                	expect(prueba.porcentajeSal).to eq(19)
+
+			expect(prueba.to_s).to eq("\tPor 100g\tIR (por 100g)\n" +
 					 "Valor energético:\t??/??\t??%\n" +
 					 "Grasas:\t21.7g\t31%\n" +
 					 "  Saturadas:\t2.6g\t13%\n" +
@@ -58,5 +61,31 @@ RSpec.describe Etiqueta do
 					 "Sal:\t1.15g\t19%\n" +
 					 "Vitaminas:\t0.0g\t-\n" +
 					 "Minerales:\t0.0g\t-\n")
+		end
+	end
+
+	context "cuando se divide en porciones" do 
+		
+		it "se inicializa correctamente" do
+			prueba = Etiqueta.new("Magdalena",21.7,2.6,54.9,29.0,4.8,1.15,0.0,0.0,0.0,0.0,0.0,0.0,0.0)
+			prueba.dividirEnPorciones(16,60.0)
+			expect(prueba.nombre).to eq("Magdalena")
+			expect(prueba.grasas).to eq(21.7)
+			expect(prueba.grasasSaturadas).to eq(2.6)
+			expect(prueba.hidratosDeCarbono).to eq(54.9)
+			expect(prueba.azucares).to eq(29.0)
+			expect(prueba.proteinas).to eq(4.8)
+			expect(prueba.sal).to eq(1.15)
+			expect(prueba.grasasMonoinsaturadas).to eq(0.0)
+			expect(prueba.grasasPoliinsaturadas).to eq(0.0)
+			expect(prueba.polialcoholes).to eq(0.0)
+			expect(prueba.almidon).to eq(0.0)
+			expect(prueba.fibraAlimentaria).to eq(0.0)
+			expect(prueba.vitaminas).to eq(0.0)
+			expect(prueba.minerales).to eq(0.0)
+			expect(prueba.porciones).to eq(16)
+			expect(prueba.cantidadPorPorcion).to eq(60.0)
+		end
+
 	end
 end
