67f713198b8071630ca467f3e30c063d2f918e9b
Jorge González Cabrera <alu0101038490@ull.edu.es>
2018-11-10 17:30:31 +0000

    Creado el método push y así evitar el fallo de las pruebas. Pendiente de comprobaciones.

--- lib/tdd/list.rb
+++ lib/tdd/list.rb
@@ -4,4 +4,20 @@ class List
 
 	attr_accessor :tail, :head
 
+	def push(valor)
+		if valor.class == Node
+			valor.next = nil
+			valor.prev = @tail
+			@tail.next = valor unless @tail.nil?
+			@tail = valor
+			@head = valor if @head.nil?
+		else
+			addedNode = Node.new(valor)
+			addedNode.prev = @tail
+			@tail.next = addedNode unless @tail.nil?
+			@tail = addedNode
+			@head = addedNode if @head.nil?
+		end	
+	end
+
 end
