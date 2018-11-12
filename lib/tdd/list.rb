5b024ad36c2e3980162105cb023137634b1640a2
Jorge González Cabrera <alu0101038490@ull.edu.es>
2018-11-12 17:23:06 +0000

    Pruebas arregladas sobre método insert de la clase List.

--- lib/tdd/list.rb
+++ lib/tdd/list.rb
@@ -2,7 +2,11 @@ class List
 
 	Node = Struct.new(:value, :next, :prev)
 
-	attr_accessor :tail, :head
+	attr_accessor :tail, :head, :size
+
+	def initialize
+		@size = 0
+	end
 
 	def push_back(valor)
 		if valor.class == Node
@@ -18,6 +22,7 @@ class List
 			@tail = addedNode
 			@head = addedNode if @head.nil?
 		end	
+		@size = @size + 1
 	end
 
 	def push_front(valor)
@@ -34,18 +39,57 @@ class List
 			@head = addedNode
 			@tail = addedNode if @tail.nil?
 		end	
+		@size = @size + 1
 	end
 
 	def pop_back
 		@tail = @tail.prev unless @tail == nil
 		@tail.next = nil unless @tail == nil
 		@head = nil if @tail == nil
+		if @size > 0
+			@size = @size - 1
+		end
 	end
 
 	def pop_front
 		@head = @head.next unless @head == nil
 		@head.prev = nil unless @head == nil
 		@tail = nil if @head == nil
+		if @size > 0
+			@size = @size - 1
+		end
+	end
+
+	def insert (position, valor)
+		if position.is_a? Integer
+			if position == 0 
+				push_front(valor)
+			elsif position == @size
+				push_back(valor)
+			elsif position < @size && position > 0
+				if valor.class == Node
+					iterator = @head
+					position.times do |i|
+						iterator = iterator.next
+					end		
+					valor.prev = iterator.prev
+					valor.next = iterator
+					iterator.prev = valor
+					valor.prev.next = valor
+				else
+					addedNode = Node.new(valor)
+					iterator = @head
+					position.times do |i|
+						iterator = iterator.next
+					end		
+					addedNode.prev = iterator.prev
+					addedNode.next = iterator
+					iterator.prev = addedNode
+					addedNode.prev.next = addedNode
+				end
+				@size = @size + 1
+			end
+		end
 	end
 
 end
