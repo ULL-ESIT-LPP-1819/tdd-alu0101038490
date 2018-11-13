e11e49a0837269944839b5231836c0584cdcdbbd
Jorge González Cabrera <alu0101038490@ull.edu.es>
2018-11-13 14:14:42 +0000

    Reorganización del código.

--- lib/tdd/list.rb
+++ lib/tdd/list.rb
@@ -5,40 +5,36 @@ class List
 	attr_reader :tail, :head, :size
 
 	def initialize
+		@tail = nil
+		@head = nil
 		@size = 0
 	end
 
 	def push_back(valor)
 		if valor.class == Node
-			valor.next = nil
-			valor.prev = @tail
-			@tail.next = valor unless @tail.nil?
-			@tail = valor
-			@head = valor if @head.nil?
+			addedNode = valor
 		else
 			addedNode = Node.new(valor)
-			addedNode.prev = @tail
-			@tail.next = addedNode unless @tail.nil?
-			@tail = addedNode
-			@head = addedNode if @head.nil?
-		end	
+		end
+		addedNode.next = nil
+		addedNode.prev = @tail
+		@tail.next = addedNode unless @tail.nil?
+		@tail = addedNode
+		@head = addedNode if @head.nil?	
 		@size = @size + 1
 	end
 
 	def push_front(valor)
 		if valor.class == Node
-			valor.prev = nil
-			valor.next = @head
-			@head.prev = valor unless @head.nil?
-			@head = valor
-			@tail = valor if @tail.nil?
+			addedNode = valor
 		else
 			addedNode = Node.new(valor)
-			addedNode.next = @head
-			@head.prev = addedNode unless @head.nil?
-			@head = addedNode
-			@tail = addedNode if @tail.nil?
-		end	
+		end
+		addedNode.prev = nil
+		addedNode.next = @head
+		@head.prev = addedNode unless @head.nil?
+		@head = addedNode
+		@tail = addedNode if @tail.nil?	
 		@size = @size + 1
 	end
 
@@ -68,25 +64,18 @@ class List
 				push_back(valor)
 			elsif position < @size && position > 0
 				if valor.class == Node
-					iterator = @head
-					position.times do |i|
-						iterator = iterator.next
-					end		
-					valor.prev = iterator.prev
-					valor.next = iterator
-					iterator.prev = valor
-					valor.prev.next = valor
+					addedNode = valor
 				else
 					addedNode = Node.new(valor)
-					iterator = @head
-					position.times do |i|
-						iterator = iterator.next
-					end		
-					addedNode.prev = iterator.prev
-					addedNode.next = iterator
-					iterator.prev = addedNode
-					addedNode.prev.next = addedNode
 				end
+				iterator = @head
+				position.times do |i|
+					iterator = iterator.next
+				end		
+				addedNode.prev = iterator.prev
+				addedNode.next = iterator
+				iterator.prev = addedNode
+				addedNode.prev.next = addedNode
 				@size = @size + 1
 			end
 		end
